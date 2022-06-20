0e60e6ac4005623271285c7e9115a21b022ffd2f
Chanyoung <chanyoung.cedu@gmail.com>
2022-06-20 21:58:32 +0900

    feat(model): make resource model

---
+++ src/models/resource.ts
@@ -0,0 +1,37 @@
+export interface IResource {
+  id: string;
+  type: ResourceType;
+  title: string;
+  source?: string;
+  copyWith(options?: Partial<IResource>): Resource;
+}
+
+export type ResourceType = 'url' | 'image';
+
+export default class Resource implements IResource {
+  constructor(
+    readonly id: string,
+    readonly type: 'url' | 'image',
+    readonly title: string,
+    readonly source: string,
+  ) {}
+
+  copyWith(options?: Partial<IResource>): Resource {
+    return new Resource(
+      options?.id ?? this.id,
+      options?.type ?? this.type,
+      options?.title ?? this.title,
+      options?.source ?? this.source,
+    );
+  }
+
+  static fromTitle(type: ResourceType, title: string, source?: string): IResource {
+    const now = Date.now();
+    return new Resource(
+      `${now}-${type}-${title}`,
+      type,
+      title,
+      source ? source : title,
+    );
+  }
+}
